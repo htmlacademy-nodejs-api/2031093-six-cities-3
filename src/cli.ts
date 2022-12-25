10a9db718b8687c430257b9f2221c719e2d17353
VitalSTU <kolysvit@mail.ru>
2022-12-25 23:34:55 +0300

    1.18.4-14 Import command added to CLI application launching module

--- src/cli.ts
+++ src/cli.ts
@@ -3,9 +3,10 @@ import { argv } from 'process';
 import CLIApplication from './app/cli-application';
 import HelpCommand from './cli-command/help-command';
 import VersionCommand from './cli-command/version-command';
+import ImportCommand from './cli-command/import-command.js';
 
 const myManager = new CLIApplication();
 myManager.registerCommands([
-  new HelpCommand, new VersionCommand
+  new HelpCommand, new VersionCommand, new ImportCommand
 ]);
 myManager.processCommand(argv);
