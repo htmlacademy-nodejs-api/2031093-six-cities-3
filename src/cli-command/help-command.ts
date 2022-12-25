02953695212812165641bd0e1a00a00777610be3
VitalSTU <kolysvit@mail.ru>
2022-12-25 20:20:29 +0300

    1.18.4-3 Help command refactoring

--- src/cli-command/help-command.ts
+++ src/cli-command/help-command.ts
@@ -8,13 +8,13 @@ export default class HelpCommand implements CliCommandInterface {
         Программа для подготовки данных для REST API сервера.
 
         Пример:
-            main.ts --<command> [--arguments]
+            cli.js --<command> [--arguments]
 
         Команды:
-            --version:                   # выводит номер версии
-            --help:                      # печатает этот текст
-            --import <path>:             # импортирует данные из TSV
-            --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
+            --version:                       # выводит номер версии
+            --help:                          # печатает этот текст
+            --import <filepath>:             # импортирует данные из TSV
+            --generate <n> <filepath> <url>  # генерирует произвольное количество тестовых данных
         `);
   }
 }
