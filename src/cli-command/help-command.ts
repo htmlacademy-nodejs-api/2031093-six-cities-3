b92650ed45b0b0d10a519a3b835bdf882a9c01f0
VitalSTU <kolysvit@mail.ru>
2022-12-26 01:05:38 +0300

    1.18.7 Chalk implemented

--- src/cli-command/help-command.ts
+++ src/cli-command/help-command.ts
@@ -1,3 +1,5 @@
+import chalk from 'chalk';
+
 import { CliCommandInterface } from './cli-command.interface.js';
 
 export default class HelpCommand implements CliCommandInterface {
@@ -8,13 +10,13 @@ export default class HelpCommand implements CliCommandInterface {
         Программа для подготовки данных для REST API сервера.
 
         Пример:
-            cli.ts --<command> [--arguments]
+        ${chalk.yellowBright('cli.ts ')}${chalk.yellowBright('--')}${chalk.green('<')}${chalk.yellowBright('command')}${chalk.green('> ')}${chalk.green('[')}${chalk.yellowBright('--arguments')}${chalk.green(']')}
 
         Команды:
-            --version:                       # выводит номер версии
-            --help:                          # печатает этот текст
-            --import <filepath>:             # импортирует данные из TSV
-            --generate <n> <filepath> <url>  # генерирует произвольное количество тестовых данных
+            ${chalk.yellowBright('--version')}:                       # выводит номер версии
+            ${chalk.yellowBright('--help')}:                          # печатает этот текст
+            ${chalk.yellowBright('--import ')}${chalk.green('<')}${chalk.white('filepath')}${chalk.green('>')}:             # импортирует данные из TSV
+            ${chalk.yellowBright('--generate ')}${chalk.green('<')}${chalk.blue('n')}${chalk.green('> <')}${chalk.white('filepath')}${chalk.green('> <')}${chalk.underline.blueBright('url')}${chalk.green('>')}  # генерирует произвольное количество тестовых данных
         `);
   }
 }
