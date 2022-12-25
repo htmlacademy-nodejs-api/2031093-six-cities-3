771756a23c9b8d7ebb364fb1add1825a30c2dd5f
VitalSTU <kolysvit@mail.ru>
2022-12-25 21:02:00 +0300

    1.18.4-6 Command processor (executor) added

--- src/app/cli-application.ts
+++ src/app/cli-application.ts
@@ -6,6 +6,7 @@ type ParsedCommand = {
 
 export default class CLIApplication {
   private commands: {[propertyName: string]: CliCommandInterface} = {};
+  private defaultCommand = '--help';
 
   private parseCommand(cliArguments: string[]): ParsedCommand {
     const parsedCommand: ParsedCommand = {};
@@ -29,5 +30,17 @@ export default class CLIApplication {
       acc[cliCommand.name] = cliCommand;
       return acc;
     }, this.commands);
+  }
+
+  public getCommand(commandName: string): CliCommandInterface {
+    return this.commands[commandName] ?? this.commands[this.defaultCommand];
+  }
+
+  public processCommand(argv: string[]): void {
+    const parsedCommand = this.parseCommand(argv);
+    const [commandName] = Object.keys(parsedCommand);
+    const command = this.getCommand(commandName);
+    const commandArguments = parsedCommand[commandName] ?? [];
+    command.execute(...commandArguments);
   }
 }
