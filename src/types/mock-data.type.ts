5ce7c520895f2fb7cfa04d00884cfe380bdc9bab
VitalSTU <kolysvit@mail.ru>
2022-12-30 09:29:33 +0300

    2.5. MockData type created.

---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,11 @@
+export type MockData = {
+  users: string[];
+  emails: string[];
+  avatars: string[];
+  titles: string[];
+  descriptions: string[];
+  cities: string[];
+  offerImages: string[];
+  offerTypes: string[];
+  categories: string[];
+};
