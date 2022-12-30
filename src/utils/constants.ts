3ea09c9dd33ef04b062b52f15872823f3ef52db7
VitalSTU <kolysvit@mail.ru>
2022-12-30 10:23:45 +0300

    2.7. Offer generator class created.

---
+++ src/utils/constants.ts
@@ -0,0 +1,15 @@
+export const RATING_MIN = 1;
+export const RATING_MAX = 5;
+export const RATING_FLOATING_POIN_LEN = 1;
+
+export const MAX_ADULTS_MIN = 1;
+export const MAX_ADULTS_MAX = 10;
+
+export const BEDROOMS_MIN = 1;
+export const BEDROOMS_MAX = 8;
+
+export const PRICE_MIN = 100;
+export const PRICE_MAX = 100000;
+
+export const COMMENTS_QTY_MIN = 0;
+export const COMMENTS_QTY_MAX = 4;
