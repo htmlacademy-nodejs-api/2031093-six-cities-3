3c39b360001f94cd3fd96ca9d7a73995bb64f165
VitalSTU <kolysvit@mail.ru>
2022-12-30 12:12:46 +0300

    2.16. *.tsv file reader class refactoring: *.tsv file stream reader code added.

--- src/common/file-reader/tsv-file-reader.ts
+++ src/common/file-reader/tsv-file-reader.ts
@@ -1,4 +1,5 @@
 import EventEmitter from 'events';
+import { createReadStream } from 'fs';
 
 import { FileReaderInterface } from './file-reader.interface.js';
 
@@ -8,6 +9,27 @@ export default class TSVFileReader extends EventEmitter implements FileReaderInt
   }
 
   public async read(): Promise<void> {
-    // TODO TSV file stream reader code
+      const stream = createReadStream(this.filename, {
+        highWaterMark: 16384, // 16KB
+        encoding: 'utf-8',
+      });
+
+      let lineRead = '';
+      let endLinePosition = -1;
+      let importedRowCount = 0;
+
+      for await (const chunk of stream) {
+        lineRead += chunk.toString();
+
+        while ((endLinePosition = lineRead.indexOf('\n')) >= 0) {
+          const completeRow = lineRead.slice(0, endLinePosition + 1);
+          lineRead = lineRead.slice(++endLinePosition);
+          importedRowCount++;
+
+          this.emit('line', completeRow);
+        }
+      }
+
+      this.emit('end', importedRowCount);
   }
 }
