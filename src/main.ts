8e5aed2e1c0a07a6cfb867c7c08d98877afcc621
VitalSTU <kolysvit@mail.ru>
2023-01-12 21:36:20 +0300

    3.19. Dependencies container created

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
-import Application from './app/application.js';
+import { Container } from 'inversify';
+
 import LoggerService from './common/logger/logger.service.js';
 import ConfigService from './common/config/config.service.js';
+import Application from './app/application.js';
+import { LoggerInterface } from './common/logger/logger.interface.js';
+import { ConfigInterface } from './common/config/config.interface.js';
+import { Component } from './types/component.types.js';
 
-const logger = new LoggerService();
-const config = new ConfigService(logger);
+const applicationContainer = new Container();
+applicationContainer.bind<Application>(Component.Application).to(Application);
+applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService);
+applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService);
 
-const application = new Application(logger, config);
+const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
