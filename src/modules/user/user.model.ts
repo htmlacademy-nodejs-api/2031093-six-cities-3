cee6246d8a30599a1f5a5559eff596f0ce17f205
VitalSTU <kolysvit@mail.ru>
2023-01-16 22:53:50 +0300

    4.11. Timestamps added to user document model

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -2,7 +2,10 @@ import * as Mongoose from 'mongoose';
 
 import { User } from '../../types/user.type.js';
 
-export interface UserDocument extends User, Mongoose.Document {}
+export interface UserDocument extends User, Mongoose.Document {
+  createdAt: Date,
+  updatedAt: Date,
+}
 
 const userSchema = new Mongoose.Schema({
   name: String,
@@ -10,6 +13,8 @@ const userSchema = new Mongoose.Schema({
   avatarPath: String,
   password: String,
   type: String,
+}, {
+  timestamps: true,
 });
 
 export const UserModel = Mongoose.model<UserDocument>('User', userSchema);
