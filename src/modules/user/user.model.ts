5831a4dc51afdeb997f2b8d222fd702496b961ce
VitalSTU <kolysvit@mail.ru>
2023-01-16 23:30:49 +0300

    4.12-4.13. Fields config and validation added to user document model

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -8,11 +8,32 @@ export interface UserDocument extends User, Mongoose.Document {
 }
 
 const userSchema = new Mongoose.Schema({
-  name: String,
-  email: String,
-  avatarPath: String,
-  password: String,
-  type: String,
+  name: {
+    type: String,
+    required: true,
+    minlength: [1, 'Min length for name is 1 symbol'],
+    maxlength: [15, 'Max length for name is 15 symbols'],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
+  },
+  avatarPath: {
+    type: String,
+    match: [/[^\s]+(\.(jpg|png))$/i, 'File extension must be "jpg" or "png"'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, 'Min length for password is 6 symbols'],
+    maxlength: [12, 'Max length for password is 12 symbols'],
+  },
+  type: {
+    type: String,
+    required: true,
+  },
 }, {
   timestamps: true,
 });
