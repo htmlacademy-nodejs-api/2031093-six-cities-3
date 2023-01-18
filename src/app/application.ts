6f4f3be02efd5fbf511731b248695758b4d89c7c
VitalSTU <kolysvit@mail.ru>
2023-01-18 22:26:14 +0300

    TEST DATA. Entities test data added.

--- src/app/application.ts
+++ src/app/application.ts
@@ -6,6 +6,8 @@ import { ConfigInterface } from '../common/config/config.interface.js';
 import { DatabaseInterface } from '../common/database-client/database.interface.js';
 import { Component } from '../types/component.types.js';
 import { getURI } from '../utils/db.js';
+import { UserModel } from '../modules/user/user.entity.js';
+import { OfferModel } from '../modules/offer/offer.entity.js';
 
 @injectable()
 export default class Application {
@@ -28,5 +30,37 @@ export default class Application {
     );
 
     await this.databaseClient.connect(uri);
+
+    const user = await UserModel.create({
+      name: 'Keks',
+      email: 'test@email.org',
+      password: 'secret',
+      avatarPath: 'keks.JpG',
+      userType: 'pro'
+     });
+   console.log(user);
+   const offer = await OfferModel.create({
+     name: 'Keks',
+
+    title: 'title title title',
+    description: 'description description description description',
+    postDate: '2023-01-18T19:16:23.704Z',
+    city: 'Amsterdam',
+    previewImage: 'previewImage',
+    images: 'images',
+    isPremium: true,
+    isFavorite: false,
+    rating: 4.4,
+    type: 'hotel',
+    maxAdults: 3,
+    bedrooms: 5,
+    price: 55000,
+    categories: [],
+    userId: user,
+    commentsQuantity: 0,
+    latitude: 48.85661,
+    longitude: 2.351499,
+    });
+  console.log(offer);
   }
 }
