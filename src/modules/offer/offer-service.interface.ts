dc18b5745e8fbff403cdbf4f4652d892c9d49fab
VitalSTU <kolysvit@mail.ru>
2023-01-26 19:06:40 +0300

    5.9. OfferService interface extended

--- src/modules/offer/offer-service.interface.ts
+++ src/modules/offer/offer-service.interface.ts
@@ -2,8 +2,17 @@ import { DocumentType } from '@typegoose/typegoose';
 
 import { OfferEntity } from './offer.entity.js';
 import CreateOfferDto from './dto/create-offer.dto.js';
+import UpdateOfferDto from './dto/update-offer.dto.js';
 
 export interface OfferServiceInterface {
   create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
   findById(id: string): Promise<DocumentType<OfferEntity> | null>;
+  updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null>;
+  deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  find(): Promise<DocumentType<OfferEntity>[]>;
+  findPremium(isPremium: boolean, count?: number): Promise<DocumentType<OfferEntity>[]>;
+  findFavorite(isFavorite: boolean, count?: number): Promise<DocumentType<OfferEntity>[]>;
+  changeFavoriteStstus(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null>;
+  incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  exists(documentId: string): Promise<boolean>;
 }
