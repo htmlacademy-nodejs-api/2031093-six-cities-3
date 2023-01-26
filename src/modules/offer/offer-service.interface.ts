826da5dafde7adcbb970a0d7c69cbb0971aa0257
VitalSTU <kolysvit@mail.ru>
2023-01-26 19:37:11 +0300

    5.10. OfferService class extended

--- src/modules/offer/offer-service.interface.ts
+++ src/modules/offer/offer-service.interface.ts
@@ -10,9 +10,9 @@ export interface OfferServiceInterface {
   updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null>;
   deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
   find(): Promise<DocumentType<OfferEntity>[]>;
-  findPremium(isPremium: boolean, count?: number): Promise<DocumentType<OfferEntity>[]>;
-  findFavorite(isFavorite: boolean, count?: number): Promise<DocumentType<OfferEntity>[]>;
-  changeFavoriteStstus(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null>;
+  findPremium(count?: number): Promise<DocumentType<OfferEntity>[]>;
+  findFavorite(count?: number): Promise<DocumentType<OfferEntity>[]>;
+  setFavoriteStatus(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null>;
   incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null>;
   exists(documentId: string): Promise<boolean>;
 }
--- src/modules/offer/offer.service.ts
+++ src/modules/offer/offer.service.ts
@@ -2,13 +2,16 @@ import { inject, injectable } from 'inversify';
 import { DocumentType, types } from '@typegoose/typegoose';
 
 import CreateOfferDto from './dto/create-offer.dto.js';
+import UpdateOfferDto from './dto/update-offer.dto.js';
 import { OfferEntity } from './offer.entity.js';
 import { OfferServiceInterface } from './offer-service.interface.js';
 import { LoggerInterface } from '../../common/logger/logger.interface.js';
 import { Component } from '../../types/component.types.js';
+import { DEFAULT_OFFER_COUNT } from './offer.constant.js';
 
 @injectable()
 export default class OfferService implements OfferServiceInterface {
+
   constructor(
     @inject(Component.LoggerInterface) private readonly logger: LoggerInterface,
     @inject(Component.OfferModel) private readonly offerModel: types.ModelType<OfferEntity>
@@ -23,8 +26,59 @@ export default class OfferService implements OfferServiceInterface {
 
   public async findById(id: string): Promise<DocumentType<OfferEntity> | null> {
     return this.offerModel
-    .findById(id)
-    .populate(['userId'])
-    .exec();
+      .findById(id)
+      .populate(['userId'])
+      .exec();
+  }
+
+  public async updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, dto, {new: true})
+      .populate(['userId'])
+      .exec();
+  }
+
+  public async deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndDelete(offerId)
+      .exec();
+  }
+
+  public async find(): Promise<DocumentType<OfferEntity>[]> {
+    return this.offerModel
+      .find()
+      .populate(['userId'])
+      .exec();
+  }
+
+  public async findPremium(count?: number): Promise<DocumentType<OfferEntity>[]> {
+    const limit = count ?? DEFAULT_OFFER_COUNT;
+    return this.offerModel
+      .find({isPremium: true}, {}, {limit})
+      .populate(['userId'])
+      .exec();
+  }
+
+  public async findFavorite(count?: number): Promise<DocumentType<OfferEntity>[]> {
+    const limit = count ?? DEFAULT_OFFER_COUNT;
+    return this.offerModel
+      .find({isFavorite: true}, {}, {limit})
+      .populate(['userId'])
+      .exec();
+  }
+
+  public async setFavoriteStatus(offerId: string, {isFavorite}: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, {'$set': {isFavorite}}).exec();
+  }
+
+  public async incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, {'$inc': {commentsQuantity: 1}}).exec();
+  }
+
+  public async exists(documentId: string): Promise<boolean> {
+    return (await this.offerModel
+      .exists({_id: documentId})) !== null;
   }
 }
