083e006601d9bc07319967023e1c9f47995b4e95
VitalSTU <kolysvit@mail.ru>
2023-01-26 20:05:24 +0300

    5.13. Comment service added

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import Application from './app/application.js';
 import { applicationContainer } from './app/application.container.js';
 import { userContainer } from './modules/user/user.container.js';
 import { offerContainer } from './modules/offer/offer.container.js';
+import { commentContainer } from './modules/comment/comment.container.js';
 import { Component } from './types/component.types.js';
 
 const mainContainer = Container.merge(
   applicationContainer,
   userContainer,
-  offerContainer
+  offerContainer,
+  commentContainer
 );
 
 async function bootstrap() {
