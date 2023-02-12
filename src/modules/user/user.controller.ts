15a95af95581e8711f6423ff87377360f499c0e1
VitalSTU <kolysvit@mail.ru>
2023-02-12 19:00:25 +0300

    6.8. UserController class created

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,28 @@
+import { inject, injectable } from 'inversify';
+import { Request, Response } from 'express';
+
+import { LoggerInterface } from '../../common/logger/logger.interface.js';
+import { Controller } from '../../common/controller/controller.js';
+import { Component } from '../../types/component.types.js';
+import { HttpMethod } from '../../types/http-method.enum.js';
+import CreateUserDto from './dto/create-user.dto.js';
+
+@injectable()
+export default class UserController extends Controller {
+
+  constructor(
+    @inject(Component.LoggerInterface) logger: LoggerInterface,
+  ) {
+    super(logger);
+    this.logger.info('Register routes for UserController…');
+
+    this.addRoute({path: '/register', method: HttpMethod.Post, handler: this.create});
+  }
+
+  public create(
+    _req: Request<Record<string, unknown>, Record<string, unknown>, CreateUserDto>,
+    _res: Response
+  ): void {
+    throw new Error('[UserController] Oops');
+  }
+}
