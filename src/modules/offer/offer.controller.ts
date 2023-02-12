5bffa08e894531508da5fd3614ffb15172385a89
VitalSTU <kolysvit@mail.ru>
2023-02-12 20:45:03 +0300

    6.11. Offer index processor code added to CategoryController

--- src/modules/offer/offer.controller.ts
+++ src/modules/offer/offer.controller.ts
@@ -2,15 +2,18 @@ import { inject, injectable } from 'inversify';
 import { Request, Response } from 'express';
 
 import { LoggerInterface } from '../../common/logger/logger.interface.js';
+import { OfferServiceInterface } from './offer-service.interface.js';
 import { Controller } from '../../common/controller/controller.js';
 import { Component } from '../../types/component.types.js';
 import { HttpMethod } from '../../types/http-method.enum.js';
+import { StatusCodes } from 'http-status-codes';
 
 @injectable()
 export default class OfferController extends Controller {
 
   constructor(
-    @inject(Component.LoggerInterface) logger: LoggerInterface
+    @inject(Component.LoggerInterface) logger: LoggerInterface,
+    @inject(Component.OfferServiceInterface) private readonly offerService: OfferServiceInterface
   ) {
     super(logger);
 
@@ -22,19 +25,20 @@ export default class OfferController extends Controller {
     this.addRoute({path: '/', method: HttpMethod.Delete, handler: this.delete});
   }
 
-  public index(req: Request, res: Response): void {
+  public async index(_req: Request, res: Response): Promise<void> {
+    const offers = await this.offerService.find();
+    this.send(res, StatusCodes.OK, offers);
+  }
+
+  public create(_req: Request, _res: Response): void {
     // Код обработчика
   }
 
-  public create(req: Request, res: Response): void {
+  public update(_req: Request, _res: Response): void {
     // Код обработчика
   }
 
-  public update(req: Request, res: Response): void {
-    // Код обработчика
-  }
-
-  public delete(req: Request, res: Response): void {
+  public delete(_req: Request, _res: Response): void {
     // Код обработчика
   }
 }
