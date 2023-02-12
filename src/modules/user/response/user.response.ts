3c6122b9bb9783a89a55c572daa8ba23744a1d4d
VitalSTU <kolysvit@mail.ru>
2023-02-12 23:35:40 +0300

    6.28. User creation added to UserController.

---
+++ src/modules/user/response/user.response.ts
@@ -0,0 +1,16 @@
+import {Expose} from 'class-transformer';
+import { UserType } from '../../../types/user-type.enum';
+
+export default class UserResponse {
+  @Expose()
+  public name!: string;
+
+  @Expose()
+  public email!: string;
+
+  @Expose()
+  public avatarPath?: string;
+
+  @Expose()
+  public userType!: UserType;
+}
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,17 +1,25 @@
 import { inject, injectable } from 'inversify';
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 
 import { LoggerInterface } from '../../common/logger/logger.interface.js';
+import { UserServiceInterface } from './user-service.interface.js';
+import { ConfigInterface } from '../../common/config/config.interface.js';
 import { Controller } from '../../common/controller/controller.js';
 import { Component } from '../../types/component.types.js';
 import { HttpMethod } from '../../types/http-method.enum.js';
+import { StatusCodes } from 'http-status-codes';
+import { fillDTO } from '../../utils/common.js';
 import CreateUserDto from './dto/create-user.dto.js';
+import HttpError from '../../common/errors/http-error.js';
+import UserResponse from './response/user.response.js';
 
 @injectable()
 export default class UserController extends Controller {
 
   constructor(
     @inject(Component.LoggerInterface) logger: LoggerInterface,
+    @inject(Component.UserServiceInterface) private readonly userService: UserServiceInterface,
+    @inject(Component.ConfigInterface) private readonly configService: ConfigInterface,
   ) {
     super(logger);
     this.logger.info('Register routes for UserController…');
@@ -20,10 +28,24 @@ export default class UserController extends Controller {
   }
 
   public async create(
-    _req: Request<Record<string, unknown>, Record<string, unknown>, CreateUserDto>,
-    _res: Response,
-    _next: NextFunction
+    {body}: Request<Record<string, unknown>, Record<string, unknown>, CreateUserDto>,
+    res: Response,
   ): Promise<void> {
-    throw new Error('[UserController] Oops');
+    const existsUser = await this.userService.findByEmail(body.email);
+
+    if (existsUser) {
+      throw new HttpError(
+        StatusCodes.CONFLICT,
+        `User with email «${body.email}» exists.`,
+        'UserController'
+      );
+    }
+
+    const result = await this.userService.create(body, this.configService.get('SALT'));
+    this.send(
+      res,
+      StatusCodes.CREATED,
+      fillDTO(UserResponse, result)
+    );
   }
 }
