870311ac4c62a2372e7c531b8f654c56687d5009
VitalSTU <kolysvit@mail.ru>
2023-02-12 19:09:25 +0300

    6.10. Controllers registration added to Application. WIP

--- src/app/application.ts
+++ src/app/application.ts
@@ -5,6 +5,7 @@ import express, { Express } from 'express';
 import { LoggerInterface } from '../common/logger/logger.interface.js';
 import { ConfigInterface } from '../common/config/config.interface.js';
 import { DatabaseInterface } from '../common/database-client/database.interface.js';
+import { ControllerInterface } from '../common/controller/controller.interface.js';
 import { Component } from '../types/component.types.js';
 import { getURI } from '../utils/db.js';
 
@@ -15,9 +16,14 @@ export default class Application {
   constructor(
     @inject(Component.LoggerInterface) private logger: LoggerInterface,
     @inject(Component.ConfigInterface) private config: ConfigInterface,
-    @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface
+    @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface,
+    @inject(Component.UserController) private userController: ControllerInterface
   ) {
     this.expressApp = express();
+  }
+
+  public initRoutes() {
+    this.expressApp.use('/users', this.userController.router);
   }
 
   public async init() {
@@ -34,6 +40,7 @@ export default class Application {
 
     await this.databaseClient.connect(uri);
 
+    this.initRoutes();
     this.expressApp.listen(this.config.get('PORT'));
     this.logger.info(`Server started on http://localhost:${this.config.get('PORT')}`);
   }
