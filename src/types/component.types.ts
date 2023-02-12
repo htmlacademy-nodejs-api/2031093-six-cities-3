ea0cb7e231993197fc443a5ead5294f36fdebede
VitalSTU <kolysvit@mail.ru>
2023-02-12 20:26:38 +0300

    6.7. Symbol for OfferController added to Component type

--- src/types/component.types.ts
+++ src/types/component.types.ts
@@ -10,4 +10,5 @@ export const Component = {
   CommentServiceInterface: Symbol.for('CommentServiceInterface'),
   CommentModel: Symbol.for('CommentModel'),
   UserController: Symbol.for('UserController'),
+  OfferController: Symbol.for('OfferController'),
 } as const;
