a1c24e1f3e2f69ed7c0a65efbcc7a9900f92dae0
VitalSTU <kolysvit@mail.ru>
2023-02-19 19:48:31 +0300

    8.9. Automatic comment author userId field filling added to comment controller.

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -35,9 +35,10 @@ export default class CommentController extends Controller {
   }
 
   public async create(
-    {body}: Request<object, object, CreateCommentDto>,
+    req: Request<object, object, CreateCommentDto>,
     res: Response
   ): Promise<void> {
+    const {body} = req;
 
     if (!await this.offerService.exists(body.offerId)) {
       throw new HttpError(
@@ -47,7 +48,7 @@ export default class CommentController extends Controller {
       );
     }
 
-    const comment = await this.commentService.create(body);
+    const comment = await this.commentService.create({...body, userId: req.user.id});
     await this.offerService.incCommentCount(body.offerId);
     this.created(res, fillDTO(CommentResponse, comment));
   }
