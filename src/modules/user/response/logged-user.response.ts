40dd02340805ff4f8524b05026aa8cb641d68e82
VitalSTU <kolysvit@mail.ru>
2023-02-21 00:25:09 +0300

    9.12. LoggedUserResponse refactoring.

--- src/modules/user/response/logged-user.response.ts
+++ src/modules/user/response/logged-user.response.ts
@@ -1,9 +1,21 @@
 import { Expose } from 'class-transformer';
 
+import { UserType } from '../../../types/user-type.enum.js';
+
 export default class LoggedUserResponse {
+
   @Expose()
   public token!: string;
 
   @Expose()
+  public name!: string;
+
+  @Expose()
   public email!: string;
+
+  @Expose()
+  public avatarPath!: string;
+
+  @Expose()
+  public userType!: UserType;
 }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -104,7 +104,10 @@ export default class UserController extends Controller {
       {email: user.email, id: user.id}
     );
 
-    this.ok(res, fillDTO(LoggedUserResponse, {email: user.email, token}));
+    this.ok(res, {
+      ...fillDTO(LoggedUserResponse, user),
+      token
+    });
   }
 
   public async uploadAvatar(req: Request, res: Response) {
@@ -114,6 +117,14 @@ export default class UserController extends Controller {
   }
 
   public async checkAuthenticate(req: Request, res: Response) {
+    if (! req.user) {
+      throw new HttpError(
+        StatusCodes.UNAUTHORIZED,
+        'Unauthorized',
+        'UserController'
+      );
+    }
+
     const user = await this.userService.findByEmail(req.user.email);
 
     this.ok(res, fillDTO(LoggedUserResponse, user));
